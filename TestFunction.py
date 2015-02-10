69fa4da58f525272ef2b6f80e391692834534155
laurajauch <laura.jauch@my.wheaton.edu>
2015-02-10 15:52:46 -0600

    Needs Function::solution()

--- TestFunction.py
+++ TestFunction.py
@@ -1,6 +1,6 @@
 import Function
 import PoissonFormulation
-#import Var
+import VarFactory
 #import Solution
 import BF
 import Mesh
@@ -12,9 +12,8 @@ y1 = Function.Function_yn(1)
 z = Function.Function_constant(0)
 
 class TestFunction(unittest.TestCase):
-    """Test x(), y(), vectorize(), evaluate() """
-    
 
+    """Test x(), y(), vectorize(), evaluate() """
     def testComponent(self):
         self.assertAlmostEqual(4,Function.Function_vectorize(x1, y1).x().evaluate(2,3),delta =1e-12)
         self.assertAlmostEqual(3,Function.Function_vectorize(x1, y1).y().evaluate(2,3),delta =1e-12)
@@ -46,10 +45,20 @@ class TestFunction(unittest.TestCase):
         self.assertEqual("y",Function.Function_vectorize(y1, z).x().displayString())
         self.assertEqual("(y,0)",Function.Function_vectorize(y1, z).displayString())
 
-    """ Test composedFunction(), constant(), normal(), solution(), xn(), yn() """
+    """ Test composedFunction(), constant(), xn(), yn() """
     def testEval(self):
         self.assertAlmostEqual(3, Function.Function_xn(1).evaluate(3, 2), delta=1e-12)
         self.assertAlmostEqual(2, Function.Function_yn(1).evaluate(3, 2), delta=1e-12)
         self.assertAlmostEqual(12, Function.Function_constant(12).evaluate(2, 2), delta = 1e-12)
         x3 = Function.Function_vectorize(z ,z)
         self.assertAlmostEqual(0.0, Function.Function_composedFunction(x1, x3).evaluate(2, 0),delta=1e-12)
+
+    """Test solution()"""
+    def testSolution(self):
+        #p = VarFactory.fluxVar("Hello")
+        #poissonForm = PoissonFormulation.PoissonFormulation(2, True)
+        #poissonBF = poissonForm.bf()
+        #mesh = MeshFactory.MeshFactory_rectilinearMesh(poissonBF,[1.0,1.0],[2,3], 1)
+        #solution = Solution.projectOntoMesh(
+	#self.assertAlmostEqual(z.l2norm(mesh) , Function.Function_solution(p, Solution.Solution_solution()), 1e-12)
+        pass
